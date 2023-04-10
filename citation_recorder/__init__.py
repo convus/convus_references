9c763aa587f9dffe32af54185396169974e552e6
sethherr <seth.william.herr@gmail.com>
2023-04-10 15:18:32 -0700

    wip

--- citation_recorder/__init__.py
+++ citation_recorder/__init__.py
@@ -1,3 +1,31 @@
+import yaml 
+
 class CitationRecorder:
+    attributes = [
+        "pay_access",
+        "title",
+        "authors",
+        "description",
+        "published_at",
+        "updated_at",
+        "word_count",
+        "doi",
+        "publisher",
+        "cannonical_url"
+    ]
+    main_attributes = {
+        "recorder_schema_version": "1",
+        "record_path": "",
+        "convus_id": "",
+        "url": "",
+    }
+
     def default_dict():
-        return {"recorder_schema_version": 1, "record_path": "test"}
+        # merge main_attributes, source_attributes and convus_attributes
+        dd = CitationRecorder.main_attributes | {"source_attributes": dict.fromkeys(CitationRecorder.attributes, "")} | {"convus_attributes": {}}
+        return dd
+    
+    def dictionary_to_yaml(dictionary):
+        # convert dictionary to yaml
+        return yaml.dump(dictionary, sort_keys=True)
+    
--- tests/test_recorder.py
+++ tests/test_recorder.py
@@ -1,9 +1,48 @@
 from citation_recorder import CitationRecorder  
 
-def test_something():
-    assert 1 == 1
+def test_default_dictionary():
+    default_dict = {
+        "recorder_schema_version": "1",
+        "record_path": "",
+        "convus_id": "",
+        "url": "",
+        "convus_attributes": {},
+        "source_attributes": {
+            "pay_access": "",
+            "title": "",
+            "authors": "",
+            "description": "",
+            "published_at": "",
+            "updated_at": "",
+            "word_count": "",
+            "doi": "",
+            "publisher": "",
+            "cannonical_url": ""
+        }
+    }
+    target_keys = ["recorder_schema_version", "record_path"]
+    assert CitationRecorder.default_dict() == default_dict
 
-def test_anotherthing():
-    target_keys = ["recorder_schema_version", "record_path"]  
-    # print(CitationRecorder.default_dict())
-    assert list(CitationRecorder.default_dict().keys()) == target_keys
+def test_dictionary_to_yaml():
+    target = """
+convus_attributes: {}
+convus_id: ''
+record_path: ''
+recorder_schema_version: '1'
+source_attributes:
+    authors: ''
+    cannonical_url: ''
+    description: ''
+    doi: ''
+    pay_access: ''
+    published_at: ''
+    publisher: ''
+    title: ''
+    updated_at: ''
+    word_count: ''
+url: ''
+    """
+    result = CitationRecorder.dictionary_to_yaml(CitationRecorder.default_dict())
+    print(result)
+    print(target)
+    assert result == target
