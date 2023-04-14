281dae4c672e45db661edf4937a148d2667d5925
sethherr <seth.william.herr@gmail.com>
2023-04-14 11:11:06 -0700

    grabbing meta attributes successfully

--- requester/__init__.py
+++ requester/__init__.py
@@ -1,6 +1,51 @@
-class Requester:
-    def __init__(self, url):
-        self.url = url
+import requests
+from bs4 import BeautifulSoup
 
-    def party():
-        return "party"
+class Requester(object):
+    def __init__(self, url, record_path=None, convus_id=None, convus_attributes=None):
+        self.url = url
+        self.record_path = record_path
+        self.convus_id = convus_id
+        self.convus_attributes = convus_attributes
+        self.soup = None
+
+    def get_url(self):
+        page = requests.get(self.url)
+        return page
+    
+    # I'm not sure if this is the best way to do this. Memoization?
+    def fetch_soup(self):
+        if self.soup == None:
+            self.soup = BeautifulSoup(self.get_url().text, 'html.parser')
+        return self.soup
+    
+    def meta_attrs(self, soup):
+        meta_elements = soup.find_all('meta')
+        attrs = {}
+        ignored_meta_keys = ['viewport', 'csrf-token', 'csrf-param', 'google-site-verification', 'robots']
+        for meta in meta_elements:
+            # print(meta.attrs)
+            if 'name' in meta.attrs:
+                key = meta.attrs['name']
+            elif 'property' in meta.attrs:
+                key = meta.attrs['property']
+            elif 'itemprop' in meta.attrs:
+                key = meta.attrs['itemprop']
+            else:
+                print("No name or property in meta element: ", meta.attrs)
+                continue
+            if key in ignored_meta_keys:
+                continue
+
+            attrs[key] = meta.attrs['content']
+        return attrs
+
+    def citation_attrs(self):
+        source_attributes = self.meta_attrs(self.fetch_soup())
+        attrs = {'reference_schema_version': '0.1',
+                 'record_path': self.record_path,
+                 'url': self.url,
+                 'convus_id': self.convus_id,
+                 'convus_attributes': self.convus_attributes,
+                 'source_attributes': source_attributes}
+        return attrs
--- tests/test_requester.py
+++ tests/test_requester.py
@@ -1,9 +1,37 @@
-from requester import CitationSpider
-
 # Following along this:
 # https://stackoverflow.com/questions/6456304/scrapy-unit-testing
 
-def test_recorder():
-    url = "https://www.example.com"
-    requester = Requester(url)
-    assert requester.url == url
+import betamax
+import requests
+# from betamax.fixtures.unittest import BetamaxTestCase
+
+with betamax.Betamax.configure() as config:
+    config.cassette_library_dir = 'tests/cassettes'
+    config.preserve_exact_body_bytes = True
+
+session = requests.Session()
+recorder = betamax.Betamax(session)
+
+from requester import Requester
+
+def test_meta_attributes(betamax_recorder):
+    with recorder.use_cassette("mendo_voice"):
+        url = "https://mendovoice.com/2023/02/fort-braggs-new-crisis-team-gets-thumbs-up-from-controversial-homeless-expert"
+        requester = Requester(url)
+        assert requester.url == url
+        meta_attrs = requester.meta_attrs(requester.fetch_soup())
+        assert meta_attrs["description"] == 'The first in series from reporter Frank Hartzell.\nShare this:TwitterFacebook'
+        target_keys = ['description', 'og:locale', 'og:type', 'og:title', 'og:description', 'og:url', 'og:site_name', 'article:publisher', 'article:published_time', 'article:modified_time', 'og:image', 'og:image:width', 'og:image:height', 'og:image:type', 'author', 'twitter:card', 'twitter:creator', 'twitter:site', 'generator', 'news_keywords', 'original-source', 'syndication-source', 'standout', 'msapplication-TileImage', 'logo', 'datePublished', 'dateModified', 'image']
+        assert len(meta_attrs.keys()) == len(target_keys)
+
+def test_citation_attributes(betamax_recorder):
+    with recorder.use_cassette("mendo_voice"):
+        url = "https://mendovoice.com/2023/02/fort-braggs-new-crisis-team-gets-thumbs-up-from-controversial-homeless-expert"
+        requester = Requester(url)
+        citation_attrs = requester.citation_attrs()
+        print(citation_attrs)
+        print(citation_attrs.keys())
+        assert(list(citation_attrs.keys())) == ['reference_schema_version', 'record_path', 'url', 'convus_id', 'convus_attributes', 'source_attributes']
+        target_source_keys = ['description', 'og:locale', 'og:type', 'og:title', 'og:description', 'og:url', 'og:site_name', 'article:publisher', 'article:published_time', 'article:modified_time', 'og:image', 'og:image:width', 'og:image:height', 'og:image:type', 'author', 'twitter:card', 'twitter:creator', 'twitter:site', 'generator', 'news_keywords', 'original-source', 'syndication-source', 'standout', 'msapplication-TileImage', 'logo', 'datePublished', 'dateModified', 'image']
+        assert len(citation_attrs["source_attributes"].keys()) == len(target_source_keys)
+
